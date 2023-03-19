eb318f8d4504e3006fca73f5b3dc997857572706
Subtext Pro <hello@subtext.pro>
2023-03-18 20:32:32 -0700

    todo route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+todos = [
+  { id: 1, title: 'Buy groceries' },
+  { id: 2, title: 'Do laundry' },
+  { id: 3, title: 'Clean the house' }
+]
+
+get '/todos' do
+  content_type :json
+  todos.to_json
+end
+
+
   
   # Add your routes here
   get "/" do
